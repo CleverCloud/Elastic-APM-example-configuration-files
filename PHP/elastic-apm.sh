8bfd097dbbeb34e338790147698d194f315bcf3f
julia <julia.navarro@clever-cloud.com>
2023-08-16 22:48:55 +0200

    replace yml file by shell
    Grabbing environment variables in yaml won't work for PHP apps, so instead we'll create a yml file dynamically when deploying

---
+++ PHP/elastic-apm.sh
@@ -0,0 +1,19 @@
+# Check that all necessary environment variables are defined
+if [ -z "$ELASTIC_APM_SERVER_URL" ] || \
+[ -z "$ELASTIC_APM_SECRET_TOKEN" ]; then
+  echo "Some environment variables are missing. Please set them before running this script."
+  exit 1
+fi
+
+# Create the YAML file
+cat <<EOL > $APP_HOME/elastic-apm.yml
+
+server_url: $ELASTIC_APM_SERVER_URL
+secret_token: $ELASTIC_APM_SECRET_TOKEN
+
+EOL
+
+echo "elastic-apm.yml file created successfully."
+
+# Give appropriate permissions
+chmod +rwx litestream.yml
